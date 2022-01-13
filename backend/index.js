7cd22c138796aafada9b45f95094b4d26b907e58
E-Kuru <evandalleaublop@gmail.com>
2022-01-13 14:54:09 +0100

    Bug adjusted

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const port = 5000
+const session = require("express-session")
+const passport = require("./config/passport")
 const { dbConnect } = require("./config/db")
 const users = require('./routes/users')
 const coment = require('./routes/coments')
@@ -11,9 +13,19 @@ dbConnect()
 
 app.use(cors({
     origin: 'http://localhost:3000',
+    credentials: true
 }))
 
 app.use(express.json())
+
+app.use(session({
+  secret: 'MyAwesomeSecret',
+  resave: true,
+  saveUninitialized: false 
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use('/tweet', tweet)
 app.use('/users', users)
