0e0e80f63aaac35d960ea91d5abe7e7b7edc4fb9
karimou cisse <karimou.cisse1@gmail.com>
2022-01-13 11:42:23 +0100

    user and comment collection

---
+++ backend/models/Coment.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require("mongoose")
+
+const ComentSchema = Schema({
+
+    content: {
+        type: String,
+        required: true
+    },
+    user: {
+            type: Schema.Types.ObjectId, ref: "User"
+    },
+    tweets: [{ 
+        type: Schema.Types.ObjectId, ref: "Tweet" 
+    }],
+   
+}, {
+    timestamps: true
+})
+
+const Coment = model('Coment', ComentSchema)
+
+module.exports = Coment
