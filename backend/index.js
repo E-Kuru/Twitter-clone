6ad9387a2992a8d6b5a64eeed520a44e7d8afdc3
karimou cisse <karimou.cisse1@gmail.com>
2022-01-13 14:49:36 +0100

    commit

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require("cors")
 const port = 5000
 const { dbConnect } = require("./config/db")
+
 const users = require('./routes/users')
 const coment = require('./routes/coments')
 
@@ -16,7 +17,7 @@ app.use(express.json())
 
 app.use('/tweet', tweet)
 app.use('/users', users)
-app.use('coments', coment)
+app.use('/coments', coment)
 
 
 app.listen(port, () => {
