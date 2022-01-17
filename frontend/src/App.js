ef99d5fa977f0cdad77b064510f6c1dd2094209f
karimou cisse <karimou.cisse1@gmail.com>
2022-01-17 09:39:22 +0100

    add homepages ang login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { UserConnectProvider } from './contexts/usersConnect'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <UserConnectProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/login" element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
+    </UserConnectProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/contexts/usersConnect.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState } from "react";
+
+const UsersConnectContext = createContext({})
+
+const UserConnectProvider = props => {
+    const [user, setUser] = useState()
+    const userURL = "http://localhost:5000/users"
+    const value = {
+        user: user,
+        setUser: setUser
+    }
+
+    return (
+        <UsersConnectContext.Provider value= {value}>
+            {props.children}
+        </UsersConnectContext.Provider>
+    )
+
+}
+
+export {
+    UserConnectProvider,
+    UsersConnectContext
+}
